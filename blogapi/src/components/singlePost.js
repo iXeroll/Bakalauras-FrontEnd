eceeb74f992091e7214267d685559896ad0f5b2b
iXeroll <evaldas1999@gmail.com>
2022-05-09 04:09:28 +0300

    Added author lookup for single view posts

--- blogapi/src/components/singlePost.js
+++ blogapi/src/components/singlePost.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axiosInstance from "../axios";
 import { useParams } from "react-router-dom";
 import { Button, Container, Grid, Typography } from "@mui/material";
+import { Divider } from "@mui/material";
 
 export default function Post() {
   const { id } = useParams();
@@ -13,17 +14,12 @@ export default function Post() {
     axiosInstance.get("post/" + id).then((res) => {
       setData({ posts: res.data });
       console.log(res.data);
+      // Get User Data for showing shit
+      // axiosInstance.get("user/" + res.data.author).then((res) => {
+      //   setUser(res.data);
+      // });
     });
   }, [setData]);
-
-  console.log(data.posts.author);
-
-  useEffect(() => {
-    axiosInstance.get("user/", +data.posts.author).then((res) => {
-      setUser(res.data);
-      console.log(res.data);
-    });
-  }, [setUser]);
 
   return (
     <React.Fragment>
@@ -56,12 +52,10 @@ export default function Post() {
             </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <Typography variant="h2" align="center">
-            Review vieta
-          </Typography>
-        </Grid>
       </Grid>
+      <Typography mt={4} variant="h2" align="center">
+        Review vieta
+      </Typography>
     </React.Fragment>
   );
 }
